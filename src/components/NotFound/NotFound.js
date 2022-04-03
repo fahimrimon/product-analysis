821e6dea06ea744a0dead3e0225101315f0775d6
Fahim Rimon <fahimrimon21@gmail.com>
2022-04-04 01:25:53 +0600

    Route set up+

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.css';
+const NotFound = () => {
+    return (
+        <div className='NotFound'>
+            <h2>Your location is not available, Please try again! </h2>
+            <h5>404</h5>
+        </div>
+    );
+};
+
+export default NotFound;
